256d23a6e62476e02e356a1c0a2d976e3718bb29
Daniel Estrada <destrada22@miners.utep.edu>
2020-03-02 14:46:23 -0700

    attendance

---
+++ java/src/SortOfSort.java
@@ -0,0 +1,11 @@
+public class SortOfSort {
+
+
+    public static void sortOfSort(int[] num) {
+        int j = 0;
+        for (int i = 0; i < num.length; i++) {//find greatest
+
+            }
+
+            }
+}
